1b9d65e6be7aef22b88484c7001182efca3f8fa4
madhavlata <madhav10.500@gmail.com>
2023-09-03 13:29:17 +0530

    sidcup to chemineers version 1 done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var crsr = document.querySelector('#cursor')
 var blur = document.querySelector("#cursor-blur")
 
 document.addEventListener("mousemove",function(dets){
-    crsr.style.left = dets.x+"px"
+    crsr.style.left = dets.x+25+"px"
     crsr.style.top = dets.y+"px"
 
     blur.style.left = dets.x-250+"px"
@@ -15,11 +15,15 @@ h4all.forEach(function(elem){
         crsr.style.scale = 3
         crsr.style.border = "1px solid #fff"
         crsr.style.backgroundColor = "transparent"
+        elem.style.color = "#80d6f5"
+        elem.style.scale = 1.1
     })
     elem.addEventListener("mouseleave",function(){
         crsr.style.scale=1;
-        crsr.style.border="0px solid #95C11E";
-        crsr.style.backgroundColor = "#95C11E";
+        crsr.style.border="0px solid #80d6f5";
+        crsr.style.backgroundColor = "#80d6f5";
+        elem.style.color = "#fff"
+        elem.style.scale = 1
     })
 })
 
@@ -84,8 +88,8 @@ gsap.from("#colon1", {
     scrollTrigger:{
         trigger:"#colon1",
         scroller:"body",
-        start:"top 55%",
-        end:"top 45%",
+        start:"top 75%",
+        end:"top 65%",
         scrub:4,
     }
 })
@@ -96,8 +100,8 @@ gsap.from("#colon2", {
     scrollTrigger:{
         trigger:"#colon2",
         scroller:"body",
-        start:"top 55%",
-        end:"top 45%",
+        start:"top 100%",
+        end:"top 75%",
         scrub:4,
     }
 })
@@ -107,8 +111,15 @@ gsap.from("#page4 h1",{
     scrollTrigger:{
         trigger:"#page4 h1",
         scroller:"body",
-        start:"top 75%",
+        start:"top 95%",
         end:"top 70%",
         scrub:3,
     }
+})
+
+arrow = document.querySelector("#arrow")
+
+arrow.addEventListener("click", function(){
+    window.scrollTo({ top : window.scrollY + (document.documentElement.clientHeight * .8), behavior:'smooth' })
+    
 })
